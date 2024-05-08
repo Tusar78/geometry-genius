0b852068b7cba10ca99c7445a80b32f188fbd5b8
Tusar <tusarahmed141@gmail.com>
2024-05-09 00:40:47 +0600

    Triangle calculation done

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,5 +6,20 @@ const getId = elem => {
 const triangleInput1 = getId('triangle-input1');
 const triangleInput2 = getId('triangle-input2');
 const triangleBtn = getId('triangle-btn');
+const triangleOutput = getId('triangleOutput');
 
 
+const calculation = () => {
+    const trOneVal = triangleInput1.value.trim();
+    const trTwoVal = triangleInput2.value.trim();
+    if (!isNaN(trOneVal) && !isNaN(trOneVal)) {
+        const tr1 = Number.parseFloat(trOneVal);
+        const tr2 = Number.parseFloat(trTwoVal);
+        const HALF = 0.5;
+        const area = HALF * tr1 * tr2
+        triangleOutput.innerText = area;
+    }
+}
+
+
+triangleBtn.addEventListener('click', calculation);
