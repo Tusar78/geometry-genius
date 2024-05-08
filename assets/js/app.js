3937d009d3e0cd0de3a2f9c17b56a3368638ac5d
Tusar <tusarahmed141@gmail.com>
2024-05-08 23:48:03 +0600

    Created all necessary reference for triangle

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Reference Method 
+const getId = elem => {
+    return document.getElementById(elem);
+}
+
+const triangleInput1 = getId('triangle-input1');
+const triangleInput2 = getId('triangle-input2');
+const triangleBtn = getId('triangle-btn');
+
+
